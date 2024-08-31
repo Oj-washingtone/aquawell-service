927f8a49bc7d9dbeeff3ab8bcc3b0a24b605cab6
JALANGO WASHINGTONE <jalangowashingtone@gmail.com>
2024-08-31 15:20:26 +0300

    purified water tank lisd model update

--- src/model/PurifiedWaterTankLid.js
+++ src/model/PurifiedWaterTankLid.js
@@ -1,38 +1,32 @@
 import db_config from "../../configs/db.js";
 import { DataTypes } from "sequelize";
 
-const PurifiedWaterTankLid = db_config.define(
-  "purified_water_tank_lid",
-  {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
-    },
-
-    appId: {
-      type: DataTypes.UUID,
-      allowNull: false,
-    },
-
-    status: {
-      type: DataTypes.ENUM("open", "close"),
-      allowNull: false,
-    },
-
-    openedAt: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
-
-    closedAt: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
+const PurifiedWaterTankLid = db_config.define("purified_water_tank_lid", {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: DataTypes.UUIDV4,
   },
-  {
-    timestamps: false, // TODO: timestamp true
-  }
-);
+
+  appId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+
+  status: {
+    type: DataTypes.ENUM("open", "close"),
+    allowNull: false,
+  },
+
+  openedAt: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+
+  closedAt: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+});
 
 export default PurifiedWaterTankLid;
