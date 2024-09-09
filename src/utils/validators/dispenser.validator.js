8404554c372ba66c764ef23bb4c2ee6f04ca90dd
JALANGO WASHINGTONE <jalangowashingtone@gmail.com>
2024-09-09 16:28:39 +0300

    dispense minimum amount update 0.01

--- src/utils/validators/dispenser.validator.js
+++ src/utils/validators/dispenser.validator.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const dispenserValidator = (data) => {
   const schema = Joi.object({
-    amount: Joi.number().required().min(1),
+    amount: Joi.number().required().min(0.01),
   });
 
   return schema.validate(data);
